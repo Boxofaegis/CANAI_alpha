eb91755d78ada3b57ce1449e7876e5b5d041068c
Boxofaegis <jsk5092@yu.ac.kr>
2023-12-06 19:37:30 +0900

    CAN v0.1 add w

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import time, os,sys
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from selenium import webdriver
-from CA
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from CAN_speaker import *
 from CAN_terminal_chat import *
